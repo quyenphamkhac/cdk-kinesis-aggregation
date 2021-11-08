56a24dc32e791c41bc7d616fb327d14c29378449
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-11-08 13:52:18 +0700

    feat: update cdk shell scripts

--- scripts/cdk-deploy-stack.sh
+++ scripts/cdk-deploy-stack.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 if [[ $# -ge 1 ]]; then
-    export STAGE=$1
-    npx cdk deploy "AbcsBackend-$STAGE"
+    if [ $1 == "Dev" ] || [ $1 == "Prod" ];then
+        export STAGE=$1
+        npx cdk deploy "AbcsBackend-$STAGE"
+    else
+        echo 1>&2 "Invalid stage. Please input 'Dev' or 'Prod'."
+        exit 1
+    fi
 else
     echo 1>&2 "Provide enviroment stage as the first argument."
     exit 1
