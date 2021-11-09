312467fa63929050014089c8c67f07693cb4c5e0
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-11-09 18:10:01 +0700

    feat: add tags to resources

--- bin/abcs-backend.ts
+++ bin/abcs-backend.ts
@@ -2,6 +2,7 @@
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { AbcsBackendStack } from "../stacks/abcs-backend-stack";
+import { Tag, Tags } from "@aws-cdk/core";
 
 const envSEA = {
   account: "820710015775",
@@ -14,10 +15,14 @@ const envUS = {
 };
 
 const app = new cdk.App();
-new AbcsBackendStack(app, "AbcsBackend-Dev", {
+const devStack = new AbcsBackendStack(app, "AbcsBackend-Dev", {
   env: envUS,
 });
+Tags.of(devStack).add("ProjectName", process.env.PROJECT_NAME as string);
+Tags.of(devStack).add("Environment", process.env.STAGE as string);
 
-new AbcsBackendStack(app, "AbcsBackend-Prod", {
+const prodStack = new AbcsBackendStack(app, "AbcsBackend-Prod", {
   env: envSEA,
 });
+Tags.of(prodStack).add("ProjectName", process.env.PROJECT_NAME as string);
+Tags.of(prodStack).add("Environment", process.env.STAGE as string);
